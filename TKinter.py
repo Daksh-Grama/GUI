083a013cf90a3ca8621f58df5cbda624ad77addc
Daksh Grama <daksh.grama@gmail.com>
2025-10-11 10:01:31 +0200

    Add files via upload

---
+++ TKinter.py
@@ -0,0 +1,24 @@
+from tkinter import*
+root = Tk()
+root.geometry("200x200")
+root.config(background = "light blue")
+Delta = Button(root, text = "Welcome to GUI", bg = "dark blue", command = root.destroy)
+
+Delta.pack(side = "top")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
