a9071cfff2805dba6056643bbeb0ffd9fa51f293
Daksh Grama <daksh.grama@gmail.com>
2025-11-08 09:56:47 +0100

    Add files via upload

---
+++ Message_Box.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox
+messagebox.showwarning("Information", "THIS IS A WARNING!!!!!!!!!!!!!")
+
+messagebox.showinfo("Information", "IF YOU CONTINUE YOU WILL BE HACKED!!!!!!!!!!!!!!!!!")
+
+messagebox.showwarning("Information", "DO NOT CONTINUE!!!!!!!!!!!!!!!!!")
+messagebox.askquestion("Confirmation", "IF YOU CONTINUE YOU WILL BE HACKED!!!!!!!!!!!!!!!!!")
+
+
+
+messagebox.showerror("Information", "ERROR")
